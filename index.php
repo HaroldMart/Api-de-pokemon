675baa402b7b61f890b184ecabaeb27bc7e62864
Haroldy Martinez <93040571+HaroldMart@users.noreply.github.com>
2022-11-15 15:27:47 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PokeApi</title>
+    <link rel="stylesheet" href="css/bootstrap.css">
+</head>
+<body>
+    <div class="container mt-4">
+        <h3>Bienvenido al PokeApi</h3>
+
+        <ul>
+            <li><a href="act/eventos.php">Eventos</a></li>
+            <li><a href="act/registro_maestro.php">Registrar Maestro</a></li>
+            <li><a href="act/lista_maestro.php">Listado de Maestro</a></li>
+            <li><a href="act/iniciar_sesion.php">Iniciar Sesion</a></li>
+            <li><a href="act/registrar_pokemon.php">Registro de pokemon</a></li>
+            <li><a href="act/mis_pokemones.php">Mis Pokemones</a></li>
+        </ul>
+    </div>
+
+</body>
+</html>
